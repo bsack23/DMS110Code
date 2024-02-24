b96340a4249f5ada78e13d62cb32355309eb4f99
bsack23 <bsack23@gmail.com>
2024-02-24 13:49:47 -0500

    make web friendly

--- week05/forLoops/sketch.js
+++ week05/forLoops/sketch.js
@@ -14,6 +14,7 @@ will print the numbers 0 - 9 to the console, and stop
 
 
 */
+// make an array of javascript objects
 let colors = [
   {
     r: 25,
@@ -39,13 +40,18 @@ function setup() {
 
 function draw() {
   background(colors[0].r, colors[0].g, colors[0].b);
+  // two nested for loops to draw a grid pattern
   for (let x = 0; x <= width; x += 50) {
     for (let y = 0; y <= height; y += 50) {
+      // select a random index for the colors array
       let i = floor(random(colors.length));
+      // fill with the color and draw an ellipse
       fill(colors[i].r, colors[i].g, colors[i].b);
       ellipse(x, y, 20);
     }
   }
+  // choose the third color from the array, add alpha
+  // and draw a rectangle at the mouse pointer
   fill(colors[2].r, colors[2].g, colors[2].b, 50);
   rect(mouseX, mouseY, 70, 70, 10);
 }
